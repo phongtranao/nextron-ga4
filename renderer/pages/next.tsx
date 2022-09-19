2f01176c544a25843c0472752d3ff1ee0bfe74bf
Phong Tran <phongtranao@gmail.com>
2022-09-19 16:43:31 +0700

    dev

--- renderer/pages/next.tsx
+++ renderer/pages/next.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
+import { v4 as uuidv4 } from 'uuid';
+import ua from 'universal-analytics';
 
 function Next() {
+    const measurement_id = `G-CLHX8VZFZ3`;
+    const api_secret = `zDCU6l7ZS_-jpxSqemJyew`;
+    var visitor = ua(measurement_id, uuidv4());
+    visitor.pageview("/", "http://localhost.com", "Welcome").send();
+    visitor.screenview("Home Screen", "App Name").send()
+
+
+    fetch(`https://www.google-analytics.com/mp/collect?measurement_id=${measurement_id}&api_secret=${api_secret}`, {
+        method: "POST",
+        body: JSON.stringify({
+            client_id: uuidv4(),
+            "events": [{
+                "name": "refund",
+                "params": {
+                    "currency": "USD",
+                    "value": "9.99",
+                    "transaction_id": "ABC-123"
+                }
+            }]
+        })
+    });
   return (
     <React.Fragment>
       <Head>
