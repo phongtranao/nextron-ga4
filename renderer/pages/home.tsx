41691cc6d1c224ac26f2789e63a00ec4238470c6
Phong Tran <phongtranao@gmail.com>
2022-09-19 17:57:58 +0700

    dev

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -2,29 +2,16 @@ import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import { v4 as uuidv4 } from 'uuid';
-import ua from 'universal-analytics';
+// import ua from 'universal-analytics';
 
 function Home() {
     const measurement_id = `G-CLHX8VZFZ3`;
     const api_secret = `zDCU6l7ZS_-jpxSqemJyew`;
-    var visitor = ua(measurement_id, uuidv4());
-    visitor.pageview("/", "http://localhost.com", "Welcome").send();
-    visitor.screenview("Home Screen", "App Name").send()
 
-
-    fetch(`https://www.google-analytics.com/mp/collect?measurement_id=${measurement_id}&api_secret=${api_secret}`, {
+    fetch(`https://analytics.google.com/g/collect?v=2&tid=${measurement_id}&gtm=2oe9e0&_p=296106398&cid=567085814.1663226662&ul=en&sr=2560x1440&uaa=x86&uab=64&uafvl=Google%2520Chrome%3B105.0.5195.125%7CNot)A%253BBrand%3B8.0.0.0%7CChromium%3B105.0.5195.125&uamb=0&uam=&uap=macOS&uapv=12.5.1&uaw=0&_z=ccd.v9B&sid=1663584410&sct=5&seg=1&dl=http%3A%2F%2Flocalhost%2Fnextron-ga4%2Fresources%2Fga.html%3F_ijt%3D9uhpq575gmhchkr0oth58hs9io&dt=Title%20APPHOME&_s=1`, {
         method: "POST",
-        body: JSON.stringify({
-            client_id: uuidv4(),
-            "events": [{
-                "name": "refund",
-                "params": {
-                    "currency": "USD",
-                    "value": "9.99",
-                    "transaction_id": "ABC-123"
-                }
-            }]
-        })
+        body: "en=page_view&_ee=1\n" +
+            "en=scroll&epn.percent_scrolled=90&_et=11",
     });
 
   return (
